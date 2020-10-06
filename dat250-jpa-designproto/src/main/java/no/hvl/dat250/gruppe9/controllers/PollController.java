89579d31a832183fad8449fdc9093200e6b4a9d2
Kenneth <fossen.kenneth@gmail.com>
2020-10-06 02:24:22 +0200

    Updated PollController to have correct Create path

--- dat250-jpa-designproto/src/main/java/no/hvl/dat250/gruppe9/controllers/PollController.java
+++ dat250-jpa-designproto/src/main/java/no/hvl/dat250/gruppe9/controllers/PollController.java
@@ -28,12 +28,17 @@ public class PollController {
     public PollController(PollService pollService, UserService userService) {
         this.pollService = pollService;
         this.userService = userService;
-
     }
 
     @GetMapping("/")
     public List<FeedPoll> getAllPolls() {
         return pollService.getAll();
+    }
+
+    @PostMapping(value = "/")
+    public FeedPoll createPoll(@RequestBody FeedPoll newPoll, Long ownerId) {
+        newPoll.setOwner(userService.getUser(ownerId));
+        return pollService.addPoll(newPoll);
     }
 
     @GetMapping(value = "/{pollid}")
@@ -42,17 +47,13 @@ public class PollController {
         return pollService.getById(id);
     }
 
-    @GetMapping(value = "/{pollid}/owner") //TODO: this also return the owners password!!
+    @GetMapping(value = "/{pollid}/owner")
     public FeedUser getOwner(@PathVariable("pollid") final Long id) {
         var userid = pollService.getPollOwner(id);
         return userService.getUser(userid);
     }
 
-    @PostMapping(value = "/{pollId}") //TODO: how are we doing this??
-    public FeedPoll createPoll(@RequestBody FeedPoll newPoll, Long ownerId) {
-        newPoll.setOwner(userService.getUser(ownerId));
-        return pollService.addPoll(newPoll);
-    }
+
 
     @PostMapping(value = "/{pollid}/vote")
     public FeedVotes createVote(@RequestBody FeedVotes vote, @PathVariable Long pollid){
@@ -61,21 +62,20 @@ public class PollController {
 
     @DeleteMapping(value = "/{pollId}")
     public ResponseEntity<String> deletePoll(@PathVariable("pollId") final Long id) {
-
         if (pollService.deletePoll(id)) {
-
             return new ResponseEntity<String>("Ok",HttpStatus.OK);
         } else {
             return new ResponseEntity<String>("No Content",HttpStatus.NO_CONTENT);
         }
     }
 
-    @GetMapping(value = "/{pollId}/result") //TODO: add the result to feedpollresult, and update feedpoll to link result to poll.
-    public FeedPollResult getResult(@PathVariable("pollId") final Long pollId) {
-        var p = pollService.getById(pollId);
-        if(p != null) {
-            return pollService.generateResult(p);
+    @GetMapping(value = "/{pollId}/result")
+    public ResponseEntity<String> getResult(@PathVariable("pollId") final Long pollId) {
+        var res = pollService.getResult(pollId);
+        if (res != null) {
+            return new ResponseEntity<String>(res.toString(),HttpStatus.OK);
+        } else {
+            return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
         }
-        return null;
     }
 }
