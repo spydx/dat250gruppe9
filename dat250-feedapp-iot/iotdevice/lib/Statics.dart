e09f5fa99ac6361b331240358938f58b6ab52b64
Kenneth <fossen.kenneth@gmail.com>
2020-11-06 18:49:43 +0100

    LogoChange og Farge endring

--- dat250-feedapp-iot/iotdevice/lib/Statics.dart
+++ dat250-feedapp-iot/iotdevice/lib/Statics.dart
@@ -2,8 +2,8 @@ class Statics {
   static const API_URL = "http://localhost:8080/api/";
   static const DEVICE_AUTH = "http://localhost:8080/api/devices/authenticate";
   static String DEVICE_ROOT = "http://localhost:8080/api/devices/";
-  static const LOGO = "assets/feedapp-logo.png";
+  static const LOGO = "assets/pollhubblue.png";
   static double BUTTONW = 150;
   static double BUTTONH = 75;
-  static const FEEDAPP_TITLE = "FeedApp";
+  static const FEEDAPP_TITLE = "PollHub";
 }
